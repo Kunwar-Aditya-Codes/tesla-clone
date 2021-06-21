ab13d095966d1ae81fa4c70e4f3ed8fb0d5ad189
Kunwar Aditya <adityakunwar0147@gmail.com>
2021-06-22 02:41:18 +0530

    initial commit

---
+++ src/components/Section.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import styled from 'styled-components';
+import Fade from 'react-reveal/Fade';
+
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+      </Fade>
+      <Button>
+        <Fade bottom>
+          <ButtonGroup>
+            <LeftBtn>{leftBtnText}</LeftBtn>
+            {rightBtnText && <RightBtn>{rightBtnText}</RightBtn>}
+          </ButtonGroup>
+          <DownArrow src='/images/down-arrow.svg' />
+        </Fade>
+      </Button>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  bacground-repeat: no-repeat;
+  background-image: url('/images/model-s.jpg');
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-image: ${(props) => ` url('/images/${props.bgImage}')`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftBtn = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightBtn = styled(LeftBtn)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Button = styled.div``;
